adc777f6fa0b69cacce5f600f653ee3a352aac13
Fatima Ibrahim <2421235@iub.edu.bd>
2026-03-20 14:07:30 +0600

    new

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,34 +5,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const border =  OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromRGBO(225,225,225,1),
+                        ),
+                        borderRadius: BorderRadius.horizontal(
+                          left: Radius.circular(50),
+                        ),
+                      );
     return Scaffold(
       body: SafeArea(
         child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  'Shoes\n Collection',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 35,
-                  )
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                'Shoes\n Collection',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                )
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  prefixIcon: Icon(
+                      Icons.search,
+                      // color: Colors.blueGrey,
+                  ),
+                  border: border,
+                  enabledBorder: border,
+                  focusedBorder: border,
                 ),
               ),
-              Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: 'Search',
-                        prefixIcon: Icon(
-                            Icons.search,
-                            color: Colors.blueGrey,
-                        )
-                    )
-                  )
-              ),
-            ]
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
